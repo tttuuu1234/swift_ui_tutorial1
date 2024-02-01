fd399d1568064d09adb29263c9a161ddee7ca8e3
tttuuu1234 <yidongyi8@gmail.com>
2024-02-02 08:40:14 +0900

    Handling user input_section3

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,21 +8,36 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
-    
-    var filterdLandmarks: [Landmark] {
+    @State private var showFavoritesOnly = false
+
+
+    var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
-    
+
+
     var body: some View {
-        List(filterdLandmarks) { landmark in
-            NavigationLink {
-                LandmarkDetails(landmark: landmark)
-            } label: {
-                LandmarkRow(landmark: landmark)
+        NavigationSplitView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetails(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
             }
+            .animation(.default, value: filteredLandmarks)
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
         }
     }
 }
